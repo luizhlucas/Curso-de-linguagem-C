e8fb725b5317d034db7e86ddd8cbcc322d845fb0
luizhlucas <luizhlucaspereira@gmail.com>
2025-02-16 19:11:01 -0300

    as-3-aulas

---
+++ aula03.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+ int main() {
+    int idade = 0;
+
+    printf("Valor inicial da idade: %i.\n", idade);
+
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
+
+    printf("Idade informada: %i.\n", idade);
+ }
